456d9f8689178edce82d28fc3b52551e52afa4bb
Ugnius Vilkoncius <ugnius.vilkoncius@gmail.com>
2022-09-16 15:13:15 +0300

    Finished 4th exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array,...theArgs) {
+    for(const arg of theArgs){
+        let indexToRemove=array.indexOf(arg);
+        if(indexToRemove>=0){
+            array.splice(indexToRemove,1);
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
