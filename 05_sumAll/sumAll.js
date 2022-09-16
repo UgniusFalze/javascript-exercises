a0deb934d519fc4f41bfc2ed7dbd1a92070a9a89
Ugnius Vilkoncius <ugnius.vilkoncius@gmail.com>
2022-09-16 15:19:36 +0300

    Finished 5th exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,16 @@
-const sumAll = function() {
+const sumAll = function(a,b) {
+    if(typeof a!="number" || typeof b!="number"){
+        return "ERROR";
+    }else if(a<0||b<0){
+        return "ERROR";
+    }
+    const left= a<b?a:b;
+    const right= a<b?b:a;
+    let result=0;
+    for(let i=left;i<=right;i++){
+        result+=i;
+    }
+    return result;
 
 };
 
